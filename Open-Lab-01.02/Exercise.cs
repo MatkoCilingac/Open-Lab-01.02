f2caaf8dc99028d0ca5c497311458917b969f656
Martin <113422018+MatkoCilingac@users.noreply.github.com>
2022-09-20 08:36:18 +0200

    Update Exercise.cs

--- Open-Lab-01.02/Exercise.cs
+++ Open-Lab-01.02/Exercise.cs
@@ -1,24 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace openlab2task
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public int (int a, int b)
         {
 
-            Console.Write("Napis cislo : ");
-            int mynum = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Napis dalsie  cislo : ");
-            int mynum2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("vysledok je : ");
-            Console.Write(mynum + mynum2);
-            Console.ReadLine();
+            return (a +b)
         }
     }
 }
